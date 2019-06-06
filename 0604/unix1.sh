51ff9811d9d2b1c71bf3946995f26269cd72799e
n19005 <n19005@std.it-college.ac.jp>
2019-06-06 11:17:05 +0900

    unix1.sh

--- 0604/unix1.sh
+++ 0604/unix1.sh
@@ -8,21 +8,35 @@ function cont() {
         exit
     fi
 }
+cont
 cd $HOME/rep/unix1/
 
 git status
-cont
-git add -i
-git status
-fi
- # git add/comment/pushの選択画面ここまで
-cont
-echo -n 'comment: '
-read comment
-git commit -m "$comment"
-git log --oneline
-cont
-git push
-unix1.sh
-fi
-# git add/coment/push
+while :
+do
+    # git add/commit/pushの選択画面
+    echo '1) git add -i'
+    echo '2) git commit'
+    echo '3) git push'
+    echo '*) exit'
+    echo -n 'select?(1/2/3/*):'
+    read select
+    case "$select" in
+        #git add -i 選択
+        1)
+        git add -i
+        git status
+        ;;
+        #git commit 選択
+        2)
+        echo -n 'comment: '
+        read comment
+        git commit -m "$comment"
+        git log --oneline 3) git push
+        ;;
+    *)
+        echo 'end..'
+        exit
+        ;;
+    esac
+done

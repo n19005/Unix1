518c0a588b16fb04f4926280f1c820bb5844ef4a
n19005 <n19005@std.it-college.ac.jp>
2019-06-25 10:46:11 +0900

    演習課題

--- 0528/052804.sh
+++ 0528/052804.sh
@@ -1,1 +1,1 @@
-sudo ls -lR /var | awk "{print $9, $5}" | sort -nr | head -n 5
+sudo ls -lR /var | awk '{print $9, $5}' | sort -k 2 -nr | head -n 5
--- 0611/061101.sh
+++ 0611/061101.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #日数を計算
 day=$(date --date="2019/9/1" +'%s')
-now=$(date --date="2019/6/11" +'%s')
+now=$(date --date="now" +'%s')
 echo $(((day-now)/(24 * 60 * 60)))

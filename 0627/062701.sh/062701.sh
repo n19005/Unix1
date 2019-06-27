0cb9cb23cdb0fc103b083432d5d3ecba8c832f27
n19005 <n19005@std.it-college.ac.jp>
2019-06-27 12:57:20 +0900

    0627テスト

---
+++ 0627/062701.sh/062701.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir dir1
+mkdir dir1/dir2
+mkdir dir1/dir2/dir3
+touch ./dir2./file{01..05}
+touch ./dir3./file{06..10}
+
---
+++ 0627/062702.sh/062702.sh
@@ -0,0 +1,4 @@
+#!/bash/bin
+echo $PATH
+TEST='TEST-VALUE'
+echo $TEST
